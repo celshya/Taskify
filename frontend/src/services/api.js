58b92c0baa5848a4cf002159cf3c2dd30eab4e32
Celshya <celshya2020@gmail.com>
2024-12-16 15:50:59 +0530

    added backend api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/tasks';
+const API_URL = 'https://taskify-nh5z.onrender.com/tasks';
 
 export const getTasks = async () => axios.get(API_URL);
-    export const createTask = async (task) => axios.post(API_URL, task);
+export const createTask = async (task) => axios.post(API_URL, task);
 export const updateTask = async (id, updatedTask) => axios.put(`${API_URL}/${id}`, updatedTask);
 export const deleteTask = async (id) => axios.delete(`${API_URL}/${id}`);
